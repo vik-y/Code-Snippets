b92c2445481e0cb2dc153957e0acec02a7da274d
bobo <vikasyadavgood@gmail.com>
2014-10-09 14:42:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+##Code-Snippets
+Code some of the most relevant algorithm and use them in the Data Structures which I have in my respository
+github.com/vik-y/Data-Structures
+
+
+##Algorithm Types
+<ol>
+
+<li>Sorting Algorithms
+	<ol>
+	Completed
+	<li>Insertion Sort</li>
+	<li>Quick Sort</li>
+	</ol>
+	<ol>
+	Incomplete
+	<li>Counting Sort</li>
+	<li>Merge Sort</li>
+	<li>Bucket Sort</li>
+</li>
+
+<li>Graph (haven't started yet)</li>
+<li>Hashing Algorithms </li>
+<li>String Matching - Coming Soon </li>
+
+</ol>
+
---
+++ Sorting/insertion-sort.c
@@ -0,0 +1,53 @@
+/*
+ * Insertion Sort :-
+ * You can see how the array is modifying in each iteration. 
+ * 1. Insertion Sort is slow but it's faster than selection sort and bubble sort. 
+ * 2. There's no additional memory requirement in insertion sort.
+ * Worst Case O(n^2)
+ * Best Case O(n)
+ * Average Case O(n^2)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertionSort(int ar_size, int ar[100]){
+	int i;
+	int j;
+	int temp;
+	int new;
+
+	for(i=1;i<ar_size;i++){
+		j=i;
+		while(j>0 && ar[j-1]>ar[j]){
+			temp = ar[j-1];
+			ar[j-1] = ar[j];
+			ar[j] = temp;
+			j--;
+		}
+		for(new=0;new<ar_size;new++){
+			printf("%d", ar[new]);
+			if(new!=ar_size-1){
+				printf(" ");
+			}
+			else{
+				printf("\n");
+			}
+		}
+	}
+
+}
+
+
+
+int main(void) {
+
+	int _ar_size;
+	scanf("%d", &_ar_size);
+	int _ar[_ar_size], _ar_i;
+	for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) {
+		scanf("%d", &_ar[_ar_i]);
+	}
+	insertionSort(_ar_size, _ar);
+	return 0;
+}
+
