1ce8a29c0294ceae9cffde8493e98b38ee876183
bobo <vikasyadavgood@gmail.com>
2014-11-06 21:25:13 +0530

    indent

--- Sorting/insertion-sort.c
+++ Sorting/insertion-sort.c
@@ -11,45 +11,44 @@
 #include <stdlib.h>
 
 void insertionSort(int ar_size, int ar[100]){
-	int i;
-	int j;
-	int temp;
-	int new;
+int i;
+int j;
+int temp;
+int new;
 
-	for(i=1;i<ar_size;i++){
-		j=i;
-		while(j>0 && ar[j-1]>ar[j]){
-			//till i array is sorted (because of the way insertion sort works)
-			//We will keep shifting elements to the right till the new element fits
-			//inside the sorted list
-			temp = ar[j-1];
-			ar[j-1] = ar[j];
-			ar[j] = temp;
-			j--;
+for(i=1;i<ar_size;i++){
+	j=i;
+	while(j>0 && ar[j-1]>ar[j]){
+		//till i array is sorted (because of the way insertion sort works)
+		//We will keep shifting elements to the right till the new element fits
+		//inside the sorted list
+		temp = ar[j-1];
+		ar[j-1] = ar[j];
+		ar[j] = temp;
+		j--;
+	}
+	for(new=0;new<ar_size;new++){
+		printf("%d", ar[new]);
+		if(new!=ar_size-1){
+			printf(" ");
 		}
-		for(new=0;new<ar_size;new++){
-			printf("%d", ar[new]);
-			if(new!=ar_size-1){
-				printf(" ");
-			}
-			else{
-				printf("\n");
-			}
+		else{
+			printf("\n");
 		}
 	}
+}
 
 }
 
 
 
 int main(void) {
-
-	int _ar_size;
-	scanf("%d", &_ar_size);
-	int _ar[_ar_size], _ar_i;
-	for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) {
-		scanf("%d", &_ar[_ar_i]);
-	}
-	insertionSort(_ar_size, _ar);
-	return 0;
+		int _ar_size;
+		scanf("%d", &_ar_size);
+		int _ar[_ar_size], _ar_i;
+		for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) {
+			scanf("%d", &_ar[_ar_i]);
+		}
+		insertionSort(_ar_size, _ar);
+		return 0;
 }
