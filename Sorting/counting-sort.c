69d63bc3ba2012c9f8cd42ad70738f06918498b2
bobo <vikasyadavgood@gmail.com>
2014-11-06 21:22:49 +0530

    indent

--- Sorting/counting-sort.c
+++ Sorting/counting-sort.c
@@ -11,25 +11,25 @@
 #define RANGE 255
 
 int main(){
-  int hash[RANGE]; //Assuming that the input ranges from 0 to 255
-  int inputs, i, y;
-  for(i=0;i<RANGE;i++) hash[i]=0; //Initializing the array to prevent garbage values in the hash
-  printf("Enter the number of values:");
-  scanf("%d", &inputs);
-  printf("Enter values one after separated by a space:");
-  for(i=0;i<inputs;i++){
-    int temp;
-    scanf("%d", &temp);
-    hash[temp]+=1; //Incrementing the hash value (This is a simple implementationo f counting sort)
-  }
-
-  //hash is already sorted, now printing the values;
-
-  for(i=0;i<RANGE; i++){
-    if(hash[i]!=NULL){
-      for(y=0;y<hash[i];y++){
-        printf("%d ", i);
-      }
+    int hash[RANGE]; //Assuming that the input ranges from 0 to 255
+    int inputs, i, y;
+    for(i=0;i<RANGE;i++) hash[i]=0; //Initializing the array to prevent garbage values in the hash
+    printf("Enter the number of values:");
+    scanf("%d", &inputs);
+    printf("Enter values one after separated by a space:");
+    for(i=0;i<inputs;i++){
+        int temp;
+        scanf("%d", &temp);
+        hash[temp]+=1; //Incrementing the hash value (This is a simple implementationo f counting sort)
     }
-  }
+
+    //hash is already sorted, now printing the values;
+
+    for(i=0;i<RANGE; i++){
+        if(hash[i]!=NULL){
+          for(y=0;y<hash[i];y++){
+            printf("%d ", i);
+          }
+        }
+    }
 }
--- Sorting/quicksort.c
+++ Sorting/quicksort.c
@@ -8,44 +8,44 @@
 
 //Starting index = start, ending index = end
 void swap(int *a, int *b){
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int partition(int a[10], int start, int end){
   //NOTE: Start and End are absolute index -> starting from 0 ;
   //I took the pivot at end, pivot can be taken at the beginning also with some modification
   //in the code
-  if(start<end){
-    int pivot = a[end];
-    int swap_index = start-1;
-    int j = start;
-    for(j=start; j<end; j++){
-      if(a[j] < pivot){
-        swap_index++;
-        swap(&a[j], &a[swap_index]);
-      }
+    if(start<end){
+        int pivot = a[end];
+        int swap_index = start-1;
+        int j = start;
+        for(j=start; j<end; j++){
+            if(a[j] < pivot){
+              swap_index++;
+              swap(&a[j], &a[swap_index]);
+            }
+        }
+        swap(&a[end], &a[swap_index+1]);
+        return swap_index+1;
     }
-    swap(&a[end], &a[swap_index+1]);
-    return swap_index+1;
-  }
-  return 0;
+    return 0;
 }
 
 int quicksort(int a[10], int start, int end){
-  if(start<end){
-    int p = partition(a, start, end);
-    quicksort(a, start, p-1);
-    quicksort(a, p+1, end);
-  }
+    if(start<end){
+        int p = partition(a, start, end);
+        quicksort(a, start, p-1);
+        quicksort(a, p+1, end);
+    }
 }
 
 int main(){
-  int i;
-  int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-  quicksort(a, 0, 9);
-  for(i=0;i<10;i++){
-    printf("%d ", a[i]);
-  }
+    int i;
+    int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    quicksort(a, 0, 9);
+    for(i=0;i<10;i++){
+        printf("%d ", a[i]);
+    }
 }
