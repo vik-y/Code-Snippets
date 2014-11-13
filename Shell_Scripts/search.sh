db74135bc7a70aa4dc0802bdf897c38bd0d8a1e6
bobo <vikasyadavgood@gmail.com>
2014-11-14 01:09:51 +0530

    search fixed

--- Shell_Scripts/search.sh
+++ Shell_Scripts/search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Enter the name of the file which you want to search for in the present directory
-read filename
-directory="$pwd"
-find $directory -name  "$filename"
+
+directory="$(pwd)"
+find $directory -name  "$1"
